bcb9ac638114f09fc5c4195d4a2c7c605f3cfa4f
Mohammad Farhat <mcfarhat@gmail.com>
2021-05-03 16:34:55 +0300

    set actifit twitter account

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'Actifit_fitness'
           }}
           options={{
             height: '300',
